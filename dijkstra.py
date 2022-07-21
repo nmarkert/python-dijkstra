66c727df3fcc875f60c4edc151fe255586ac6753
Niklas Markert <niklas.markert@outlook.de>
2022-07-21 20:41:06 +0200

    Created Test Graph and implemented different Output Formats

--- dijkstra.py
+++ dijkstra.py
@@ -1,6 +1,7 @@
 # https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 import math
 from abc import ABC, abstractmethod
+from typing import List, Any
 
 
 class DijkstraNode:
@@ -18,8 +19,8 @@ class DijkstraNode:
 
 class Dijkstra(ABC):
 
-    def init_graph(self, start):
-        nodes = self.get_nodes()
+    def init_graph(self, start: Any):
+        nodes = self.all_nodes()
         graph = []
         for n in nodes:
             dijkstra_node = DijkstraNode(n)
@@ -28,7 +29,7 @@ class Dijkstra(ABC):
             graph.append(dijkstra_node)
         return graph
 
-    def get_node_with_min_dist(self, graph):
+    def get_node_with_min_dist(self, graph: List[DijkstraNode]) -> DijkstraNode:
         min_dist = math.inf
         node = None
         for n in graph:
@@ -37,19 +38,48 @@ class Dijkstra(ABC):
                 node = n
         return node
 
-    def get_dijkstra_node(self, graph, node):
+    def get_dijkstra_node(self, graph: List[DijkstraNode], node: Any) -> DijkstraNode | None:
         for n in graph:
             if n == node:
                 return n
         return None
 
-    def reformat_output(self, nodes):
-        output = []
-        for n in nodes:
-            output.append((n.obj, n.dist, n.prev))
+    def reconstruct_path(self, nodes, start_node, end_node):
+        node = self.get_dijkstra_node(nodes, end_node)
+        path = [node.obj]
+        while node != start_node:
+            node = node.prev
+            path.append(node.obj)
+        path.reverse()
+        return path
+
+    def reformat_output(self, nodes: List[DijkstraNode], output_format: str, start_node: Any = None,
+                        end_node: Any = None):
+        output = None
+        if output_format == 'dijkstra':
+            output = []
+            for node in nodes:
+                prev = None
+                if node.prev is not None:
+                    prev = node.prev.obj
+                output.append((node.obj, node.dist, prev))
+
+        elif output_format == 'single_path':
+            output = self.reconstruct_path(nodes, start_node, end_node)
+
+        elif output_format == 'all_paths':
+            output = []
+            for node in nodes:
+                output.append((node.obj, self.reconstruct_path(nodes, start_node, node)))
+
+        elif output_format == 'costs':
+            output = []
+            for node in nodes:
+                output.append((node.obj, node.dist))
+
         return output
 
-    def dijkstra_search(self, start, end):
+    def dijkstra_search(self, start, end=None, output_format='dijkstra'):
         graph = self.init_graph(start)
         visited = []
 
@@ -57,29 +87,31 @@ class Dijkstra(ABC):
             u = self.get_node_with_min_dist(graph)
             graph.remove(u)
             visited.append(u)
+            if end is not None and u == end:
+                break
 
-            for neigh in self.get_neighbors(u.obj):
-                v = self.get_dijkstra_node(graph, neigh)
+            for neighbor in self.neighbors(u.obj):
+                v = self.get_dijkstra_node(graph, neighbor)
                 if v is None:
                     continue
-                alt = u.dist + self.get_weight(u.obj, v.obj)
+                alt = u.dist + self.weight(u.obj, v.obj)
                 if alt < v.dist and u.dist != math.inf:
                     v.dist = alt
                     v.prev = u
 
-        return self.reformat_output(visited)
+        return self.reformat_output(visited, output_format, start, end)
 
     @abstractmethod
-    def get_neighbors(self, node):
+    def neighbors(self, node):
         """ Get all neighbor nodes of the given node """
         raise NotImplementedError
 
     @abstractmethod
-    def get_nodes(self):
+    def all_nodes(self):
         """ Get all nodes in the graph """
         raise NotImplementedError
 
     @abstractmethod
-    def get_weight(self, node1, node2):
+    def weight(self, node1, node2):
         """ Get the weight for going from node1 to node2 (i.e. the distance) """
         raise NotImplementedError
--- tests/simple_graph.py
+++ tests/simple_graph.py
@@ -1,17 +1,37 @@
+import math
+
+import dijkstra
+
+
 class Node:
-    def __init__(self, node_id: int):
+
+    def __init__(self, node_id: int, name: str = None):
         self.id = node_id
+        self.name = name
+
+    def __str__(self):
+        return '<Node:' + self.name + '>'
+
+    def __repr__(self):
+        return str(self)
 
 
 class Edge:
+
     def __init__(self, edge_id: int, start: Node, end: Node, weight: float):
         self.id = edge_id
         self.start = start
         self.end = end
         self.weight = weight
 
+    def __str__(self):
+        return str(self.start) + " --" + str(self.weight) + "--> " + str(self.end)
 
-class Graph:
+    def __repr__(self):
+        return str(self)
+
+
+class Graph(dijkstra.Dijkstra):
 
     def __init__(self):
         self.nodes = list()
@@ -19,18 +39,61 @@ class Graph:
         self.node_id = 0
         self.edge_id = 0
 
-    def add_node(self):
-        node = Node(self.node_id)
-        self.node_id += 1
-        self.nodes.append(node)
-        return node
+    def add_node(self, amount: int = 1, node_names: list = None):
+        nodes = []
+        if node_names is None or len(node_names) != amount:
+            node_names = [None] * amount
+        for i in range(amount):
+            node = Node(self.node_id, node_names[i])
+            self.node_id += 1
+            self.nodes.append(node)
+            nodes.append(node)
+        if amount == 1:
+            return nodes[0]
+        return tuple(nodes)
 
-    def add_edge(self, start: Node, end: Node, weight: float, bidirectional: bool = True):
+    def add_edge(self, start: Node, end: Node, weight: float = 1, bidirectional: bool = True):
         self.edges.append(Edge(self.edge_id, start, end, weight))
         self.edge_id += 1
 
         if bidirectional:
             self.edges.append(Edge(self.edge_id, end, start, weight))
             self.edge_id += 1
+
+    def all_nodes(self):
+        return self.nodes
+
+    def neighbors(self, node):
+        neighbors = []
+        for edge in self.edges:
+            if edge.start == node:
+                neighbors.append(edge.end)
+        return neighbors
+
+    def weight(self, node1, node2):
+        for e in self.edges:
+            if e.start == node1 and e.end == node2:
+                return e.weight
+        return math.inf
+
+
+if __name__ == '__main__':
+    # Test graph: https://de.wikipedia.org/wiki/Dijkstra-Algorithmus#/media/Datei:Dijkstra_Animation.gif
+    graph = Graph()
+    n1, n2, n3, n4, n5, n6 = graph.add_node(6, ['1', '2', '3', '4', '5', '6'])
+    graph.add_edge(n1, n2, 7)
+    graph.add_edge(n1, n3, 9)
+    graph.add_edge(n1, n6, 14)
+    graph.add_edge(n2, n3, 10)
+    graph.add_edge(n2, n4, 15)
+    graph.add_edge(n3, n4, 11)
+    graph.add_edge(n3, n6, 2)
+    graph.add_edge(n4, n5, 6)
+    graph.add_edge(n5, n6, 9)
+    out1 = graph.dijkstra_search(n1)
+    print(out1)
+
+    out2 = graph.dijkstra_search(n1, n6, 'costs')
+    print(out2)
 
 
