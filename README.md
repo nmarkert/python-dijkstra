cf47cef552c073dba49fcb498b9e4b6eb720a177
Niklas Markert <niklas.markert@outlook.de>
2022-09-01 15:11:54 +0200

    Added some Documentation and updated readme

--- README.md
+++ README.md
@@ -1,18 +1,77 @@
-# python-dijkstra
+# PyDijkstra
 
-Python Package for providing easy use of a dijkstra graph search algorithm. <br>
-The work in the project is still in progress, so there are some things still missing, but the main algorithm is working
+This python package provides an implementation of the  Dijkstra Search Algorithm for any kind of graphs.
+The focus of this realization lies on the usability. <br>
+If you have already written your own class which represents some sort of graph, and you want to 
+use the dijkstra algorithm for path searching in this graph, this package is extremely easy to 
+integrate. You don't have to transform your graph into another special search graph or something.
+The only things you have to do, is inheriting from the provided class and override some methods.
 
-## Todo
-- Add unittests
-- Add (at least some) documentation
+## How to Use
+**Install Package**
+```
+pip install pydijkstra
+```
 
-## Links
-https://de.wikipedia.org/wiki/Dijkstra-Algorithmus <br>
-https://github.com/jrialland/python-astar <br>
-https://realpython.com/pypi-publish-python-package/
+**Import Package and Inherit from Dijkstra Class**
+```python
+import pydijkstra
 
-## Upload on PyPi
-$ python -m build <br>
-$ twine upload -r testpypi dist/* <br>
-$ twine upload dist/*
+class MyGraph(pydijkstra.Dijkstra):
+    ...
+```
+
+**Implement the Abstract Methods**
+```python
+def all_nodes(self) -> List[Any]:
+    """ Return all nodes in the graph """
+
+def neighbors(self, node: Any) -> List[Any]:
+    """ Return all neighbor nodes of the given node """
+
+def weight(self, node1: Any, node2: Any) -> float | int:
+    """ Return the weight for going from node1 to node2 (i.e. the distance) """
+```
+*The typing 'Any' here stands for the representation of one node. So you are free in the choice
+how you want to represent a node, i.e. use your own defined class, coordinates, ...*
+
+**Use the dijkstra_search Method**
+<br>which was inherited from the Dijkstra class
+```python
+dijkstra_search(start: Any, 
+                target: Any = None, 
+                output_format: str = 'dijkstra')
+```
+- (param) **start**: Node to start the dijkstra search from.
+- (param) **target**: Target node in order to perform early stopping.
+- (param) **output_format**: String which defines the format of the output. More on this at [Output Formats](#markdown-header-output-formats).
+- (return) Result of the dijkstra search based on the output_format.
+
+### Output Formats
+For different uses you  may be need different outputs from the dijkstra search, so you can define the output of the
+function by the output_format parameter. <br>
+**Important!** The algorithm to calculate the path finding stays the same, independent of the output format. So changing
+the output format does not lead to a change in calculation cost.
+<br> <br>
+The different options:
+- `dijkstra` For each found node, it gives the previous visited node and the distance from the start node.
+The value pair for each node is represented as a dict with 'prev' and 'dist' as its keys.
+- `path` For each found node, it gives a list of nodes, which represents the shortest path from the start node.
+- `path+dist` For each found node, it gives the shortest path and the distance from the start node.
+The value pair for each node is represented as a dict with 'path' and 'dist' as its keys.
+- `target_path` Gives a list of nodes, which represents the shortest path from the start node to the target node. (Only
+                possible if target is given).
+
+If the output format defines outputs for multiple nodes (i.e. when using 'dijkstra' or 'path'), it tries to return it 
+as a dictionary with the nodes as the keys. If the node is not hashable (and so not usable as a key in the dictionary),
+it instead returns a list with tuples where the node is the first object and the output the second
+object of the tuple.
+
+### Examples
+Some examples on how the package can be used are found in the tests package. <br>
+In simple_graph.py, a new graph structure is defined, which inherits from pydijkstra.Dijkstra and implements the needed functions
+to be able to perform a dijkstra search on the graph it represents. <br>
+In nx_search.py, a search class is defined, which gets a NetworkX graph and implements the needed functions with the
+help of the functions provided by the graph, so it can serve as a path finding class for NetworkX graphs. <br>
+In test.py, some unittests for the dijkstra algorithm are defined, which use the two implemented classes in order to
+test the correctness of the dijkstra algorithm.
--- pydijkstra/dijkstra.py
+++ pydijkstra/dijkstra.py
@@ -21,17 +21,14 @@ class DijkstraNode:
 
 class Dijkstra(ABC):
 
-    def init_graph(self, start: Any):
-        nodes = self.all_nodes()
-        graph = []
-        for n in nodes:
-            dijkstra_node = DijkstraNode(n)
-            if n == start:
-                dijkstra_node.dist = 0
-            graph.append(dijkstra_node)
-        return graph
+    @staticmethod
+    def __get_node_with_min_dist(graph: List[DijkstraNode]) -> DijkstraNode:
+        """
+        Finds the DijkstraNode with the minimum distance in a given graph.
 
-    def get_node_with_min_dist(self, graph: List[DijkstraNode]) -> DijkstraNode:
+        :param graph: List of DijkstraNodes.
+        :return: DijkstraNode with the minimum distance.
+        """
         node = graph[0]
         min_dist = node.dist
         for n in graph[1:]:
@@ -40,14 +37,31 @@ class Dijkstra(ABC):
                 node = n
         return node
 
-    def get_dijkstra_node(self, graph: List[DijkstraNode], node: Any) -> DijkstraNode | None:
+    @staticmethod
+    def __get_dijkstra_node(graph: List[DijkstraNode], node: Any) -> DijkstraNode | None:
+        """
+        Finds the DijkstraNode in the graph which represents the given node.
+
+        :param graph: List of DijkstraNodes.
+        :param node: Node to which the related DijkstraNode should be found.
+        :return: DijkstraNode representing the given node.
+        """
         for n in graph:
             if n == node:
                 return n
         return None
 
-    def reconstruct_path(self, nodes: List[DijkstraNode], start_node: Any, target_node: Any):
-        node = self.get_dijkstra_node(nodes, target_node)
+    @staticmethod
+    def __reconstruct_path(nodes: List[DijkstraNode], start_node: Any, target_node: Any) -> List[Any]:
+        """
+        Reconstructs a path from a start_node to an end_node based on the given DijkstraNodes.
+
+        :param nodes: List of DijkstraNodes from which the path should be reconstructed
+        :param start_node: Node from which the path should start
+        :param target_node: Node where the path should end
+        :return: List of nodes representing the path
+        """
+        node = Dijkstra.__get_dijkstra_node(nodes, target_node)
         if node is None:
             return []
         path = [node.obj]
@@ -57,8 +71,23 @@ class Dijkstra(ABC):
         path.reverse()
         return path
 
-    def reformat_output(self, nodes: List[DijkstraNode], output_format: str, start_node: Any,
-                        target_node: Any = None) -> Any:
+    @staticmethod
+    def __reformat_output(nodes: List[DijkstraNode], output_format: str, start_node: Any,
+                          target_node: Any = None) -> Any:
+        """
+        Reformats a list of DijkstraNodes into an output defined by the output_format.
+
+        If it returns outputs for multiple nodes (i.e. when using 'dijkstra' or 'path'), it tries to return it as a
+        dictionary with the node as the key. If the node is not hashable (and so not usable as a key in the dictionary),
+        it instead returns a list with tuples where the node is the first object of the tuple and the output the second
+        object.
+
+        :param nodes: List of DijkstraNodes which defines the result of the dijkstra search.
+        :param output_format: A string which defines the format of the output.
+        :param start_node: Start node of the search. (Needed when reconstructing paths)
+        :param target_node: Target node of the search. (Needed when reconstructing the target path)
+        :return: The formatted output of the dijkstra search.
+        """
         try:
             hash(start_node)
         except TypeError:
@@ -83,28 +112,60 @@ class Dijkstra(ABC):
 
         elif output_format == 'path':
             for node in nodes:
-                append_output(node.obj, self.reconstruct_path(nodes, start_node, node.obj))
+                append_output(node.obj, Dijkstra.__reconstruct_path(nodes, start_node, node.obj))
 
         elif output_format == 'path+dist':
             for node in nodes:
-                append_output(node.obj, (self.reconstruct_path(nodes, start_node, node.obj), node.dist))
+                append_output(node.obj, {'path': Dijkstra.__reconstruct_path(nodes, start_node, node.obj),
+                                         'dist': node.dist})
 
         elif output_format == 'target_path':
             if target_node is None:
                 raise ValueError('target_path is not allowed as output_format if no target is specified!')
-            output = self.reconstruct_path(nodes, start_node, target_node)
+            output = Dijkstra.__reconstruct_path(nodes, start_node, target_node)
 
         else:
             raise ValueError(str(output_format) + ' is no valid output format!')
 
         return output
 
+    def __init_graph(self, start: Any) -> List[DijkstraNode]:
+        """
+        Initializes the graph needed for the dijkstra search.
+
+        :param start: Start node of the dijkstra search.
+        :return: List of DijkstraNodes representing the graph.
+        """
+        nodes = self.all_nodes()
+        graph = []
+        for n in nodes:
+            dijkstra_node = DijkstraNode(n)
+            if n == start:
+                dijkstra_node.dist = 0
+            graph.append(dijkstra_node)
+        return graph
+
     def dijkstra_search(self, start: Any, target: Any = None, output_format: str = 'dijkstra'):
-        unvisited = self.init_graph(start)
+        """
+        Performs a dijkstra search from a given start node.
+
+        :param start: Node to start the dijkstra search from.
+        :param target: Target node in order to perform early stopping. (Optional)
+        :param output_format: String which defines the format of the output. Options:
+            - 'dijkstra' (Default): For found each node, it gives the previous visited node and the distance from the
+                start node.
+            - 'path': For each found node, it gives a list of nodes, which represents the shortest path from the start
+                node.
+            - 'path+dist': For each found node, it gives the shortest path and the distance from the start node.
+            - 'target_path': Gives a list of nodes, which represents the shortest path from the start node to the target
+                node. (Only possible if target is given.)
+        :return: The result of the dijkstra search.
+        """
+        unvisited = self.__init_graph(start)
         visited = []
 
         while len(unvisited) > 0:
-            u = self.get_node_with_min_dist(unvisited)
+            u = Dijkstra.__get_node_with_min_dist(unvisited)
             if u.dist == math.inf:
                 # Node with min dist has inf dist -> graph is not fully connected
                 break
@@ -117,7 +178,7 @@ class Dijkstra(ABC):
                 break
 
             for neighbor in self.neighbors(u.obj):
-                v = self.get_dijkstra_node(unvisited, neighbor)
+                v = Dijkstra.__get_dijkstra_node(unvisited, neighbor)
                 if v is None:
                     # neighbor was already visited
                     continue
@@ -127,19 +188,19 @@ class Dijkstra(ABC):
                     v.dist = alt
                     v.prev = u
 
-        return self.reformat_output(visited, output_format, start, target)
-
-    @abstractmethod
-    def neighbors(self, node: Any) -> List[Any]:
-        """ Get all neighbor nodes of the given node """
-        raise NotImplementedError
+        return Dijkstra.__reformat_output(visited, output_format, start, target)
 
     @abstractmethod
     def all_nodes(self) -> List[Any]:
-        """ Get all nodes in the graph """
+        """ Return all nodes in the graph """
+        raise NotImplementedError
+
+    @abstractmethod
+    def neighbors(self, node: Any) -> List[Any]:
+        """ Return all neighbor nodes of the given node """
         raise NotImplementedError
 
     @abstractmethod
     def weight(self, node1: Any, node2: Any) -> float | int:
-        """ Get the weight for going from node1 to node2 (i.e. the distance) """
+        """ Return the weight for going from node1 to node2 (i.e. the distance) """
         raise NotImplementedError
